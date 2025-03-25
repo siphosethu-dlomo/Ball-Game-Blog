894902fab89fcbe4e54a379c915a1c5b89edd135
Siphosethu Dlomo <siphosethugdlomo@gmail.com>
2025-03-25 03:20:35 +0200

    responsive design, part 1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
-			sm: "480px",
+			xs: "320px",
+			sm: "360px",
 			smd: "640px",
 			md: "768px",
 			slg: "996px",
